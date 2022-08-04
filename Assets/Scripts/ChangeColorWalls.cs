cf0828601f5bf35bffd8dbabfa1476c313a6898c
Riku Okazaki <riku606298@yahoo.co.jp>
2022-08-04 09:59:37 +0900

    First Commit

---
+++ Assets/Scripts/ChangeColorWalls.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColorWalls : MonoBehaviour
+{
+    private GameObject obj;
+    private Color objColor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+     void OnCollisionEnter(Collision collision){
+        obj = collision.gameObject;
+        objColor = obj.GetComponent<Renderer>().material.color;
+        GetComponent<Renderer>().material.color = objColor;
+    }
+}
